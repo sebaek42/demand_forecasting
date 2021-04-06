d8d5d01a8bc0473dcbff2c317b4e3db80fb115d4
seunghun <dau4723@gmail.com>
2021-04-06 15:17:46 +0900

    web

---
+++ web/db/mysql.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+require('dotenv').config();
+
+//DB Settings
+var connection = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
+});
+
+module.exports = connection;
---
+++ web/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+const { query } = require('./db/mysql');
+var mysql = require('./db/mysql');
+var app = express();
+
+require('dotenv').config();
+
+// DB
+mysql.connect();
+
+// Settings
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname+'/public'))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+app.use('/', require('./routes/home'));
+
+
+var port = 8888;
+app.listen(port, function(){
+    console.log('server on! http://localhost:'+port)
+});
