c4f8d0ebffe3a89676d3479cf0ea1a51dac091f0
sebaek42 <sebaek@student.42seoul.kr>
2021-04-14 14:35:35 +0900

    FEATURE : geojson file uploaded

--- web/index.js
+++ web/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 const { query } = require('./db/mysql');
-var mysql = require('./db/mysql');
+// var mysql = require('./db/mysql');
 var app = express();
 
 require('dotenv').config();
 
 // DB
-mysql.connect();
+// mysql.connect();
 
 // Settings
 app.set('view engine', 'ejs');
